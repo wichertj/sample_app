a0f6e4affe964c8ec77a6478ea6aa4c6e7de6a97
Jonas Wichert <jowinote101@gmail.com>
2013-05-18 15:36:58 +0200

    Contact page added

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -40,5 +40,18 @@ describe "Static pages" do
       page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About Us")
     end
   end
+  
+  describe "Contact" do 
+  
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+    
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
 end
 
