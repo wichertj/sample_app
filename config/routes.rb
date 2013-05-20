227bcc992ababdf273637debb02fa04d0baf9a1a
Jonas Wichert <jowinote101@gmail.com>
2013-05-20 12:54:02 +0200

    Finish layout, routes and signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 SampleApp::Application.routes.draw do
+  get "user/new"
+
   root to: 'static_pages#home'
+  
+  match '/signup',  to: 'user#new'
 
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
