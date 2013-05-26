a24714762343871c7cbcbf6015a87c9eda5d6265
Jonas Wichert <jowinote101@gmail.com>
2013-05-26 17:27:14 +0200

    Finish user edit, update, index and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    name    "Jonas Wichert"
-    email   "jonas_wichert@gmx.ch"
-    password  "foobar"
+    sequence(:name) { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com" }
+    password "foobar"
     password_confirmation "foobar"
+    
+    factory :admin do
+      admin true
+    end
   end
 end
